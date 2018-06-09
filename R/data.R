ac4c5e81c7ce04baea44ae6fbe38aed983242a26
Jedidiah Carlson <jed.e.carlson@gmail.com>
2018-06-09 18:12:59 -0400

    add data objects for various subtype vectors

--- R/data.R
+++ R/data.R
@@ -3,6 +3,32 @@
 #' A dataset containing the frequencies of 96 3-mer subtypes
 #'
 #' @format A data frame with 2504 rows and 97 variables
-#'
-#' @source \url{http://www.diamondse.info/}
 "subtype_count_matrix"
+
+#' Bracket-formatted subtypes
+#' 
+#' A dataset containing the frequencies of 96 3-mer subtypes
+#'
+#' @format A 96-element vector of mutation subtypes formatted as "T[C>A]G"
+"subtypes_bracket"
+
+#' signeR-formatted subtypes
+#' 
+#' A dataset containing the frequencies of 96 3-mer subtypes
+#'
+#' @format A 96-element vector of mutation subtypes formatted as "C>A:TCG"
+"subtypes_signer"
+
+#' SomaticSignatures-formatted subtypes
+#' 
+#' A dataset containing the frequencies of 96 3-mer subtypes
+#'
+#' @format A 96-element vector of mutation subtypes formatted as "CA T.G"
+"subtypes_ss"
+
+#' YAPSA-formatted subtypes
+#' 
+#' A dataset containing the frequencies of 96 3-mer subtypes
+#'
+#' @format A 96-element vector of mutation subtypes formatted as "C>A TCG"
+"subtypes_yapsa"
--- tests/test.R
+++ tests/test.R
@@ -7,27 +7,3 @@ msm_SomaticSignatures <- format_counts(subtype_count_matrix, "SomaticSignatures"
 msm_signeR <- format_counts(subtype_count_matrix, "signeR")
 msm_maftools <- format_counts(subtype_count_matrix, "maftools")
 msm_MutationalPatterns <- format_counts(subtype_count_matrix, "MutationalPatterns")
-
-# sum(colnames(dsdat) == colnames(sigs.input)) == 96
-# 
-# ssdat <- format_counts(mu_counts, "SomaticSignatures")
-# sum(rownames(ssdat) == rownames(sca_mm)) == 96
-# 
-# signerdat <- format_counts(mu_counts, "signeR")
-# sum(colnames(signerdat) == colnames(mut)) == 96
-# # somaticsigs
-# sca_mm
-# 
-# # deconstructsigs
-# require(deconstructSigs)
-# sigs.input <- mut.to.sigs.input(mut.ref = sample.mut.ref, 
-#                                 sample.id = "Sample", 
-#                                 chr = "chr", 
-#                                 pos = "pos", 
-#                                 ref = "ref", 
-#                                 alt = "alt")
-# sigs.input
-# 
-# # signer
-# mut <- read.table(system.file("extdata","21_breast_cancers.mutations.txt",
-#                               package="signeR"), header=TRUE, check.names=FALSE)
